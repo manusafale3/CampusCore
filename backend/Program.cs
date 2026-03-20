44704581c19f1414c5405eb0bdc9b24ce55210af
manu <manusafale3@gmail.com>
2026-03-20 06:13:30 +0000

    Add retry on failure for Azure SQL

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,7 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    sqlOptions => sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)));
 
 // Register services
 builder.Services.AddScoped<AuthService>();
